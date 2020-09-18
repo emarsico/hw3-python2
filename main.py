17e0497bc6876c55463619d73360b63aa03404fd
emarsico <eam6077@psu.edu>
2020-09-18 18:37:39 +0000

    switched from floor to integer divison

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import math
-
 def digit_sum(n):
   if (n < 10):
     return n
   else:
-    last = n - (math.floor(n/10)) * 10
-    return (digit_sum(math.floor(n/10))) + last
+    last = n%10
+    n = n//10
+    return (digit_sum(n)) + last
 
 def run():
   digit = int(input("Enter an int: "))
-  print(f"sum of digits of {digit} is {digit_sum(digit)}.")
+  print(f"sum of digits of {digit} is {digit_sum(digit)+digit_sum(0)}.")
 
 if __name__ == "__main__":
   run();
